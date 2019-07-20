63127cbfe73895433b3a2ea9fc0a27eb4733ddb7
Vinson Chuong <vinsonchuong@gmail.com>
2019-07-20 14:24:36 -0700

    Add react-hot-loader

--- ui/index.js
+++ ui/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { render } from 'react-dom'
+import Ui from './ui'
 
-render(
-  <>Hello World!</>,
-  window.root
-)
+render(<Ui />, window.root)
---
+++ ui/ui.js
@@ -0,0 +1,10 @@
+import { hot } from 'react-hot-loader'
+import React from 'react'
+
+export default hot(module)(() => {
+  return (
+    <div>
+      Hello World!
+    </div>
+  )
+})
